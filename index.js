1620bcab142cda7b999b2789a7d6034866225149
Aman Gupta <coderamang02@gmail.com>
2024-01-16 22:21:33 +0530

    10change in cara

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+
 function cursor() {
   let cursor = document.querySelector(".cursor");
   let cont = document.querySelector(".wrapper");
   let prod = document.querySelectorAll(".div");
   let logo = document.querySelector(".logo");
   let products=document.querySelector(".prod")
-
+  let btn = document.querySelectorAll(".swiper-slide button")
   cont.addEventListener("mousemove", (dets) => {
     gsap.to(cursor, {
       left: dets.x+10,
       top: dets.y+10,
+    });
+  });
+  btn.forEach((item) => {
+    let vle = item.innerHTML
+    item.addEventListener("mouseenter", () => {
+      cursor.style.width = "130px";
+      cursor.style.height = "130px";
+      cursor.innerHTML = `${vle}`
+    });
+    item.addEventListener("mouseleave", () => {
+      cursor.style.width = "25px";
+      cursor.style.height = "25px";
+      cursor.innerHTML = ""
     });
   });
   prod.forEach((item) => {
@@ -19,36 +33,30 @@ function cursor() {
       });
       cursor.style.width = "100px";
       cursor.style.height = "100px";
-      cursor.style.backgroundColor = "rgba(255, 192, 203, 0.475)";
     });
     item.addEventListener("mouseleave", () => {
       cursor.style.width = "25px";
       cursor.style.height = "25px";
-      cursor.style.backgroundColor = "grey";
     });
   });
   logo.addEventListener("mousemove", () => {
     cursor.style.width = "80px";
     cursor.style.height = "80px";
-    cursor.style.backgroundColor = "grey";
   });
   logo.addEventListener("mouseleave", () => {
     cursor.style.width = "25px";
     cursor.style.height = "25px";
-    cursor.style.backgroundColor = "grey";
   });
-  // products.addEventListener("mousemove", () => {
-  //   cursor.style.width = "80px";
-  //   cursor.style.height = "80px";
-  //   cursor.style.backgroundColor = "grey";
-  //   cursor.innerHTML = "Products"
-  // });
-  // products.addEventListener("mouseleave", () => {
-  //   cursor.style.width = "25px";
-  //   cursor.style.height = "25px";
-  //   cursor.style.backgroundColor = "grey";
-  //   cursor.innerHTML = ""
-  // });
+  products.addEventListener("mousemove", () => {
+    cursor.style.width = "85px";
+    cursor.style.height = "85px";
+    cursor.innerHTML = "Products"
+  });
+  products.addEventListener("mouseleave", () => {
+    cursor.style.width = "25px";
+    cursor.style.height = "25px";
+    cursor.innerHTML = ""
+  });
 }
 cursor();
 
@@ -81,3 +89,12 @@ var swiper = new Swiper(".mySwiper",{
 })
 }
 Swwiper()
+
+function myfunction(){
+  window.open("sproduct.html")
+}
+myfunction()
+
+
+
+
